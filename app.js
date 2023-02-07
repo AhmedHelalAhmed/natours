3ac837fabd370171b6489f916cd64d1c2e157bc2
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-02-07 23:50:45 +0200

    🎉 Add post and get routes with json response

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ const app = express();
 const port = 3000;
 const OK = 200;
 app.get('/', (request, response) => {
-  response.status(OK).send('Hello from the server side!');
+  response.status(OK).json({
+    message: 'Hello from the server side!',
+    app: 'Natours',
+  });
 });
 
+app.post('/', (request, response) => {
+  response.status(OK).json({
+    message: 'You can post to this endpoint...',
+    app: 'Natours',
+  });
+});
 app.listen(port, () => {
   console.log(`listening on port ${port}...`);
 });
