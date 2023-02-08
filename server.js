fceb493ad8bf2ec47d745478d0f2bc262688914c
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-02-09 00:37:21 +0200

    Remove logs 🔇

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const dotEnv = require('dotenv');
 dotEnv.config({ path: './config.env' });
 const app = require('./app');
-console.log(process.env);
 app.listen(process.env.PORT, () => {
   console.log(`listening on port ${process.env.PORT}...`);
 });
