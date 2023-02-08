c39f89cf1f76485b840273f275dd860e74d0094d
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-02-08 21:41:06 +0200

    Refactor routes 🔧 🔧

--- app.js
+++ app.js
@@ -92,12 +92,12 @@ const deleteTour = (request, response) => {
   });
 };
 
-app.post('/api/v1/tours', createTour);
-app.get('/api/v1/tours', getAllTours);
-app.get('/api/v1/tours/:id', getTour);
-app.patch('/api/v1/tours/:id', updateTour);
-app.delete('/api/v1/tours/:id', deleteTour);
-
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 app.listen(port, () => {
   console.log(`listening on port ${port}...`);
 });
