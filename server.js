2a7e39592b792ff0eac1d47e9b4cf6a41f393882
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-02-09 20:08:04 +0200

    Add and configure mongoose 🔧

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const dotEnv = require('dotenv');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const mongoose = require('mongoose');
 
 dotEnv.config({ path: './config.env' });
+const DATABASE = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful!'));
 const app = require('./app');
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`listening on port ${process.env.PORT}...`);
 });
