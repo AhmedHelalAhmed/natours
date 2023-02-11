e1c05f93ddf43be9daa27f04d40513b1c2f050f0
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-02-11 19:10:58 +0200

    🚑️🚑️ Handle uncaught exceptions and unhandled Rejection

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const dotEnv = require('dotenv');
 const mongoose = require('mongoose');
+// must be on the top before any code execution
+process.on('uncaughtException', (error) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(error);
+  process.exit(1);
+});
 
 dotEnv.config({ path: './config.env' });
 const DATABASE = process.env.DATABASE.replace(
@@ -16,6 +22,15 @@ mongoose
   .then(() => console.log('DB connection successful!'));
 const app = require('./app');
 
-app.listen(process.env.PORT || 3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`listening on port ${process.env.PORT}...`);
+});
+
+process.on('unhandledRejection', (error) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(error);
+  // finished all the work running the shut-down
+  server.close(() => {
+    process.exit(1); //uncaught error
+  });
 });
