19195e8a52b167c586076b6fb5249e1b1b4531ee
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-03-10 11:31:55 +0200

    🔒️prevent denial of service and brute force attacks

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
@@ -8,10 +10,21 @@ const { NOT_FOUND } = require('./enums/httpResponse');
 
 const app = express();
 
+// GLOBAL MIDDLEWARES
 // middleware: required to make express work with body-parser
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// allow 100 requests for the same ip for 1 hour
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000, // 1 hour in milliseconds
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+// this rateLimit to prevent denial of service and brute force attacks
+app.use('/api', limiter);
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 app.use((request, response, next) => {
