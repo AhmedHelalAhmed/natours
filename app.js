7acdaa980566d712c59e39a9cc2f2763a43cc127
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-03-10 13:11:00 +0200

    🔒️Preventing Parameter Pollution
    npm i hpp

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -41,6 +42,20 @@ app.use(mongoSanitize());
 
 // Data sanitization against XSS (cross-site scripting attacks) - protect from html js code
 app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
