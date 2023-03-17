a2727b88b9c30b864e7cf8c7b2eefe3b8b279a3a
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-03-18 01:12:47 +0200

    ♻️ refactor using nested routes wisth express and mergeParams

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,8 +2,13 @@ const express = require('express');
 const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
 
-const router = express.Router();
+const router = express.Router({
+  mergeParams: true, // by default, each router only have access to the parameters of their specific routes,
+  // and we need to get access to tour id comes in the url
+});
 
+// POST /tour/24fsd5k/reviews
+// POST/reviews
 router
   .route('/')
   .get(authController.protect, reviewController.getAllReviews)
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
-const reviewController = require('../controllers/reviewController');
+const reviewRouter = require('./reviewRoutes');
 
 const router = express.Router();
+
+// POST /tour/24fsd5k/reviews -> nested route
+// GET /tour/24fsd5k/reviews -> get reviews
+router.use('/:tourId/reviews', reviewRouter);
 
 router.param('id', tourController.checkIfTourExists);
 router.route('/tour-statistics').get(tourController.getTourStatistics);
@@ -23,17 +27,6 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
-  );
-
-// POST /tour/24fsd5k/reviews -> nested route
-// GET /tour/24fsd5k/reviews -> get reviews
-// GET /tour/24fsd5k/reviews/ddf41dfe5 -> get review
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview
   );
 
 module.exports = router;
