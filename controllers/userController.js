6d8ccf3ca549840e183f4e4bb4ca5d218ed58cc7
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-03-18 12:08:51 +0200

    ✨ add a /me endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,7 +26,10 @@ exports.createUser = (request, response) => {
     message: 'This route is not defined! please use /signup instead',
   });
 };
-
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
